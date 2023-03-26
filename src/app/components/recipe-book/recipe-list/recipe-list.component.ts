aa35999552ffaf92db84a9d72fa28bfd456da729
Iustin Ghergu <iusting20@gmail.com>
2023-03-26 15:52:06 +0100

    created functional list

--- src/app/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'test food',
+      'some description',
+      'https://centraltransylvania.com/wp-content/uploads/2020/03/recette-des-choux-farcis-ou-sarmale-camelia-870x588-1.jpg'
+    ),
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
