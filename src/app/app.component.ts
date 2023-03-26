14003236819177988c73ca48d92ce02b41cf29da
Iustin Ghergu <iusting20@gmail.com>
2023-03-26 13:28:45 +0100

    initialised components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'assignment-iustin-47';
-}
+export class AppComponent {}
